162083b2c627ca8ac8f297f97a6a743826f7c8ce
Davidrs05 <david.r@opendeusto.es>
2024-10-31 17:06:10 +0100

    listeners que relacionan los botones con la tabla + inicio JTree

--- src/interfaces_graficas/Nueva_Rutina.java
+++ src/interfaces_graficas/Nueva_Rutina.java
@@ -9,6 +9,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -25,6 +26,9 @@ import javax.swing.JTree;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import clases_de_apyo.Rutina;
 
 
 public class Nueva_Rutina extends JFrame{
@@ -32,6 +36,7 @@ public class Nueva_Rutina extends JFrame{
 	
 	private JTable tablaRutina;
 	private DefaultTableModel modeloDatosTablaRutina;
+	private     JPanel ventana_central_MuestraRutinas;
 
 
 	public Nueva_Rutina() {
@@ -45,15 +50,9 @@ public class Nueva_Rutina extends JFrame{
         // Creamos la ventana y definimos distribucion
 		JPanel ventanaPrincipal_nuevaRutina = new JPanel(new BorderLayout());
 		JScrollPane scrollPane = new JScrollPane(this.tablaRutina);
-	    JPanel ventana_central_MuestraRutinas = new JPanel(new BorderLayout());
-		initTableRutina();
+	    ventana_central_MuestraRutinas = new JPanel(new BorderLayout());
 		
-		
-		
-		
-		
-		
-		
+		
 
 		//FUENTE-EXTERNA
 		//URL: (https://chuidiang.org/index.php?title=Uso_de_Layouts)
@@ -62,17 +61,66 @@ public class Nueva_Rutina extends JFrame{
 		JPanel layout_botones = new JPanel(new FlowLayout());
 		layout_botones.setBorder(new TitledBorder("Selecciona el tipo de entrenamiento que quieres añadir: "));
 		//
-		JButton fuerza = new JButton("Fuerza");
-		fuerza.setFocusable(false);
-		layout_botones.add(fuerza);
-		JButton cardio = new JButton("Cardio");
-		cardio.setFocusable(false);
-		layout_botones.add(cardio);
-		JButton natacion = new JButton("Natacion");
-		natacion.setFocusable(false);
-		layout_botones.add(natacion);
+		JButton boton_fuerza = new JButton("Fuerza");
+		boton_fuerza.setFocusable(false);
+		layout_botones.add(boton_fuerza);
+		
+		
+		
+		ActionListener listener_boton_fuerza = e -> {
+            ventana_central_MuestraRutinas.removeAll();
+
+			ventana_central_MuestraRutinas.add(init_tabla_fuerza());
+			
+			ventana_central_MuestraRutinas.revalidate(); // FUENTE EXTERNA: revalidate y repaing sacados con ayuda de ChatGPT
+          	ventana_central_MuestraRutinas.repaint();
+        };
+        
+        
+        boton_fuerza.addActionListener(listener_boton_fuerza);
+		
+		
+		
+		
+		
+		JButton boton_cardio = new JButton("Cardio");
+		boton_cardio.setFocusable(false);
+		layout_botones.add(boton_cardio);
+		
+		
+		ActionListener listener_boton_cardio = e -> {
+            ventana_central_MuestraRutinas.removeAll();
+
+			ventana_central_MuestraRutinas.add(init_tabla_cardio());
+			
+			ventana_central_MuestraRutinas.revalidate(); // FUENTE EXTERNA: revalidate y repaing sacados con ayuda de ChatGPT
+          	ventana_central_MuestraRutinas.repaint();
+        };
+        
+        
+        boton_cardio.addActionListener(listener_boton_cardio);
+		
+		JButton boton_natacion = new JButton("Natacion");
+		boton_natacion.setFocusable(false);
+		layout_botones.add(boton_natacion);
+		
+		ActionListener listener_boton_natacion = e -> {
+            ventana_central_MuestraRutinas.removeAll();
+
+			ventana_central_MuestraRutinas.add(init_tabla_natacion());
+			
+			ventana_central_MuestraRutinas.revalidate(); // FUENTE EXTERNA: revalidate y repaing sacados con ayuda de ChatGPT
+          	ventana_central_MuestraRutinas.repaint();
+        };
+        
+        
+        boton_natacion.addActionListener(listener_boton_natacion);
+		
+		
 		
 		ventanaPrincipal_nuevaRutina.add(layout_botones, BorderLayout.NORTH);
+		
+		
 
 //
 		
@@ -81,10 +129,16 @@ public class Nueva_Rutina extends JFrame{
 		
 		//ventana_nueva_rutina.setLayout(new GridBagLayout());
 		
-		ventanaPrincipal_nuevaRutina.add(new JTree(), BorderLayout.WEST);
-		ventanaPrincipal_nuevaRutina.add(ventana_central_MuestraRutinas);
-		JTree arbol = new JTree();
-		ventanaPrincipal_nuevaRutina.add(arbol, BorderLayout.WEST);
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode("Ejercicio");
+		CrearNodos(top);
+		JTree arbolSeleccionEjercicios = new JTree(top);
+		
+		
+		
+		
+		
+		
+		ventanaPrincipal_nuevaRutina.add(arbolSeleccionEjercicios, BorderLayout.WEST);
 		ventanaPrincipal_nuevaRutina.add(ventana_central_MuestraRutinas);
         
         
@@ -128,26 +182,115 @@ public class Nueva_Rutina extends JFrame{
 
 	}
 	
-	private void initTableRutina() {
+	private JScrollPane init_tabla_fuerza(){
         // Crear cabecera de la tabla
         Vector<String> cabeceraTabla = new Vector<String>(
-            Arrays.asList("Tipo Entrenamiento", "Ejercicio", "Musculo", "Series", "Repeticiones", "Tiempo")
+            Arrays.asList("Tipo Entrenamiento", "Musculo", "Foto", "Series", "Repeticiones")
         );
         // Crear el modelo de datos de la tabla
         this.modeloDatosTablaRutina = new DefaultTableModel(new Vector<Vector<Object>>(), cabeceraTabla);
 
         // Crear la tabla y configurar celdas editables
-        this.tablaRutina = new JTable(this.modeloDatosTablaRutina) {
-            public boolean isCellEditable(int row, int column) {
-                return column == 3 || column == 4 || column == 5;
-            }
+        JTable tablaRutina = new JTable(this.modeloDatosTablaRutina);
+        tablaRutina.setVisible(true);
+        JScrollPane scrollPane = new JScrollPane(tablaRutina);
+
+        return scrollPane;
+        
         };
 
-        // Añadir la tabla a un JScrollPane y agregar el JScrollPane al panel central
-       // JScrollPane scrollPane = new JScrollPane(this.tablaRutina);
-        //JPanel ventana_central_MuestraRutinas = new JPanel(new BorderLayout());
-        //ventana_central_MuestraRutinas.add(scrollPane, BorderLayout.CENTER);
-        
-        // Añadir el panel de la tabla al panel principal
-    }}
+    	private JScrollPane init_tabla_cardio(){
+            // Crear cabecera de la tabla
+            Vector<String> cabeceraTabla = new Vector<String>(
+                Arrays.asList("Tipo Entrenamiento", "Musculo", "Foto", "Tiempo", "Ritmo")
+            );
+            // Crear el modelo de datos de la tabla
+            this.modeloDatosTablaRutina = new DefaultTableModel(new Vector<Vector<Object>>(), cabeceraTabla);
+
+            // Crear la tabla y configurar celdas editables
+            JTable tablaRutina = new JTable(this.modeloDatosTablaRutina);
+            tablaRutina.setVisible(true);
+            JScrollPane scrollPane = new JScrollPane(tablaRutina);
+
+            return scrollPane;
+            
+            };
+            
+        	private JScrollPane init_tabla_natacion(){
+                // Crear cabecera de la tabla
+                Vector<String> cabeceraTabla = new Vector<String>(
+                    Arrays.asList("Tipo Entrenamiento", "Musculo", "Foto", "Largos", "Ritmo")
+                );
+                // Crear el modelo de datos de la tabla
+                this.modeloDatosTablaRutina = new DefaultTableModel(new Vector<Vector<Object>>(), cabeceraTabla);
+
+                // Crear la tabla y configurar celdas editables
+                JTable tablaRutina = new JTable(this.modeloDatosTablaRutina);
+                tablaRutina.setVisible(true);
+                JScrollPane scrollPane = new JScrollPane(tablaRutina);
+
+                return scrollPane;
+                
+                };
+                
+             //   #FUENTE EXTERNA: Hemos tomado como referencia esta URL:
+             //   https://docs.oracle.com/javase/tutorial/uiswing/components/tree.html
+            private void CrearNodos(DefaultMutableTreeNode top) {
+            	DefaultMutableTreeNode ejercicio = null;
+
+            	DefaultMutableTreeNode categ1 = new DefaultMutableTreeNode("Torso") ; 
+            	top.add(categ1);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Pecho");
+            	categ1.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Espalda");
+            	categ1.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Trapecios");
+            	categ1.add(ejercicio);
+            	
+            	
+            	DefaultMutableTreeNode categ2 = new DefaultMutableTreeNode("Brazos") ; 
+            	top.add(categ2);
+
+            	
+            	ejercicio = new DefaultMutableTreeNode("Biceps");
+            	categ2.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Triceps");
+            	categ2.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Hombro");
+            	categ2.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Antebrazo");
+            	categ2.add(ejercicio);
+            	
+            	DefaultMutableTreeNode categ3 = new DefaultMutableTreeNode("Piernas") ; 
+            	top.add(categ3);
+            	
+            	
+            	ejercicio = new DefaultMutableTreeNode("Cuádriceps");
+            	categ3.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Isquiotibiales");
+            	categ3.add(ejercicio);
+          
+            	ejercicio = new DefaultMutableTreeNode("Gemelos");
+            	categ3.add(ejercicio);
+            	
+            	ejercicio = new DefaultMutableTreeNode("Glúteos");
+            	categ3.add(ejercicio);
+            	
+
+
+
+            	
+            	
+            	
+            }
+                
+                
+    }
 
